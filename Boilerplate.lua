5d20fab7dcc20140ad2f56508a5b9ae8401801a7
TheJustFox <97546695+TheJustFox@users.noreply.github.com>
2026-06-27 13:56:21 +0300

    Create Boilerplate.lua for addon structure

---
+++ Boilerplate.lua
@@ -0,0 +1,17 @@
+--@Author: phoenix_a
+--@Game: Criminality, Universal
+--// ^ These 2 values can be any string, no checks
+
+local module = {}
+
+--// Called when the addon is loaded, this is where you can set up your addon
+module.init = function(libraries)
+
+end
+
+--// Called when the addon is unloaded or reloaded, clean up anything the addon did here (removing hooks, connections, etc)
+module.unload = function()
+
+end
+
+return module
